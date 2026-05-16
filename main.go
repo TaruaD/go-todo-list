7a00cb17fc023d487d867834ecb5636bf3d9aa6e
TaruaD <1799897046@qq.com>
2026-05-16 23:57:43 +0800

    数据库

--- main.go
+++ main.go
@@ -1,88 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"tod/router"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-//	func enableCORS(next http.HandlerFunc) http.HandlerFunc {
-//		return func(w http.ResponseWriter, r *http.Request) {
-//			w.Header().Set("Access-Control-Allow-Origin", "*")
-//			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-//			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-//			if r.Method == http.MethodOptions {
-//				w.WriteHeader(http.StatusOK)
-//				return
-//			}
-//			next.ServeHTTP(w, r)
-//		}
-//	}
-func main() {
-	//createHandler
-	//http.HandleFunc("/create", enableCORS(createHandler))
-	////deleteHandler
-	//http.HandleFunc("/Delete", enableCORS(deleteHandler))
-	////getallHandler
-	//http.HandleFunc("/GetAll", enableCORS(getAllHandler))
-	rMain := router.Router()
-	rMain.Run(":8080")
+// Todo Model
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
 }
 
-//func createHandler(w http.ResponseWriter, r *http.Request) {
-//	if r.Method != http.MethodPost {
-//		http.Error(w, "仅支持 POST 请求", http.StatusMethodNotAllowed)
-//		return
-//	}
-//	params := map[string]string{}
-//	err := json.NewDecoder(r.Body).Decode(&params)
-//	if err != nil {
-//		log.Println("Error decoding request body:", err)
-//		http.Error(w, "无效的请求体", http.StatusBadRequest)
-//		return
-//	}
-//	name := params["name"]
-//	id := uuid.New().String()
-//	var newTodo db.Todo = db.Todo{
-//		ID:        id,
-//		Name:      name,
-//		Completed: false,
-//	}
-//	err = db.CreateTodo(newTodo)
-//	if err != nil {
-//		log.Fatal(err)
-//		w.WriteHeader(http.StatusInternalServerError)
-//		return
-//	}
-//	w.WriteHeader(http.StatusOK)
-//}
-//func getAllHandler(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	todos, err := db.GetAllTodos()
-//	if err != nil {
-//		log.Fatal(err)
-//		w.WriteHeader(http.StatusInternalServerError)
-//		return
-//	}
-//	json.NewEncoder(w).Encode(todos)
-//
-//}
-//func deleteHandler(w http.ResponseWriter, r *http.Request) {
-//	if r.Method != http.MethodPost {
-//		http.Error(w, "仅支持 POST 请求", http.StatusMethodNotAllowed)
-//		return
-//	}
-//	params := map[string]string{}
-//	err := json.NewDecoder(r.Body).Decode(&params)
-//	if err != nil {
-//		log.Println("Error decoding request body:", err)
-//		http.Error(w, "无效的请求体", http.StatusBadRequest)
-//		return
-//	}
-//	id := params["id"]
-//	if err = db.DeleteTodo(id); err != nil {
-//		log.Fatal(err)
-//		w.WriteHeader(http.StatusInternalServerError)
-//		return
-//	}
-//	w.WriteHeader(http.StatusOK)
-//}
+func main() {
+	//数据库
+	db, err := gorm.Open(sqlite.Open("db/test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("连接失败:", err)
+	}
+	db.AutoMigrate(&Todo{})
+	fmt.Println("连接数据库成功")
+	rMain := router.Router()
+	rMain.Run(":8080")
+	V1Group := rMain.Group("/v1")
+	{
+		//添加
+		V1Group.POST("/todo", func(c *gin.Context) {})
+		//查看所有
+		V1Group.GET("/todo", func(c *gin.Context) {
+
+		})
+		//查看某个
+		V1Group.GET("todo/:id", func(c *gin.Context) {
+
+		})
+		//修改
+		V1Group.PUT("todo/:id", func(c *gin.Context) {
+
+		})
+		//删除
+		V1Group.DELETE("todo/id", func(c *gin.Context) {
+
+		})
+	}
+}
